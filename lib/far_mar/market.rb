7cb8db78b0848565ec221a737e1d8db16639c544
Katie <kab761@gmail.com>
2014-09-15 14:59:35 -0700

    convert csv to array of Market objects

--- lib/far_mar/market.rb
+++ lib/far_mar/market.rb
@@ -1,4 +1,20 @@
 module FarMar
   class Market
+    attr_reader :id, :name, :address, :city, :county, :state, :zip
+
+    def initialize(market_array)
+      @id = market_array[0]
+      @name = market_array[1]
+      @address = market_array[2]
+      @city = market_array[3]
+      @county = market_array[4]
+      @state = market_array[5]
+      @zip = market_array[6]
+    end
+
+    def self.make_many(file_path="./support/markets.csv")
+      file_contents = CSV.read(file_path)
+      file_contents.collect { |market| Market.new(market) }
+    end
   end
 end
